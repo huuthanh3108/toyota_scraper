fe0346989e695e204f05b0cb6e8ef78adc8b7421
Nguyen Huu Thanh <thanhnh@mail.nitrotech.asia>
2026-05-14 10:16:45 +0700

    first commit

---
+++ toyota_scraper/spiders/toyota_accessories.py
@@ -0,0 +1,29 @@
+import scrapy
+from datetime import datetime
+
+class ToyotaAccessorySpider(scrapy.Spider):
+    name = "toyota_accessories"
+    allowed_domains = ["danang.toyota.com.vn"]
+    start_urls = [
+        "https://danang.toyota.com.vn/phu-tung-phu-kien-chinh-hang?p=1"
+    ]
+
+    def parse(self, response):
+        products = response.css("div.phukien-home")
+
+        for product in products:
+            yield {
+                "name": product.css(".description h4::text").get(default="").strip(),
+                "price": product.css(".price-phukien span::text").get(default="").strip(),
+                "image": product.css(".img img::attr(data-src)").get(),
+                "url": response.urljoin(
+                    product.css(".img img::attr(data-src)").get()
+                ),
+                "crawl_date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            }
+
+        # pagination
+        next_page = response.css("ul.pagination li:last-child[class~='action'] a::attr(href)").get(default=None).strip()
+        print("NEXT:", next_page)
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
